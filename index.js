6d1d46bfe8eb92bdd1f0936619afe85a23a78b88
Anton-Tretiak <tretiak.ap@gmail.com>
2025-03-03 15:17:38 +0200

    upd

--- index.js
+++ index.js
@@ -5,9 +5,15 @@ const port = process.env.PORT || 3000;
 app.use(express.json());
 
 app.post('/pronature', (req, res) => {
-    console.log('Request Body:', req.body);
+  console.log('Request Body:', req.body);
+
+  res.status(200).json({
+    success: true,
+    message: "Request received successfully",
+    receivedData: req.body
+  });
 });
 
 app.listen(port, () => {
-    console.log(`Server is running`);
+  console.log(`Server is running.`);
 });
